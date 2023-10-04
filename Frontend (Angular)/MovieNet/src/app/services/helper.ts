acb75fcc0b59f545440d754de909c3e43a1c11de
shiwirockztar <shiwirockztar@gmail.com>
2023-10-04 17:20:34 -0500

    pause concept Front
    paramos el avanze de la rama de desarrollo hasta este punto, dado que para su funcionamiento ya empleamos el backend

--- Frontend (Angular)/MovieNet/src/app/services/helper.ts
+++ Frontend (Angular)/MovieNet/src/app/services/helper.ts
@@ -4,4 +4,10 @@
 export let baseNetUrl = 'http://localhost:7000';
 export let api_key = '192e0b9821564f26f52949758ea3c473';
 export let language = 'language=es-MX';
+// Popular
 export let ApiMovieDbUrl = 'https://api.themoviedb.org/3/movie/popular?';
+// Rating
+export let ApiMovieDbPopularUrl =
+  'https://api.themoviedb.org/3/movie/top_rated?';
+
+//   Watchlist varchar(50),
