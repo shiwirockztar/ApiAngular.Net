d0b9e3d7d9f5569ec24be7d2cbb2b886762ab02a
shiwirockztar <shiwirockztar@gmail.com>
2023-10-04 18:03:42 -0500

    add Repositories
    agregando las interfaces

---
+++ Backend (.NET Core)/ApiRestMovies/ApiRestMovies.Data/Repositories/IMovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using ApiRestMovies.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiRestMovies.Data.Repositories
+{
+    public interface IMovieRepository
+    {
+        Task<IEnumerable<Movie>> GetMovies(int id);
+        Task<Movie> GetMovieByTitle(string title);
+        Task<Movie> GetMovieById(int id);
+        Task<bool> InsertMovie(Movie movie);
+        Task<bool> DeleteMovie(int id);
+    }
+}
---
+++ Backend (.NET Core)/ApiRestMovies/ApiRestMovies.Data/Repositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApiRestMovies.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiRestMovies.Data.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<bool> SignIn(User user);
+        Task<bool> Login(User user);
+
+    }
+}
